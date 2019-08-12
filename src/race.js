5c89426ae98fcdbd39b4d007202648115bc93d87
tomon9086 <tomo.mouse.9086@gmail.com>
2019-08-12 18:19:25 +0900

    レースデータを取得できる

--- src/race.js
+++ src/race.js
@@ -1,12 +1,83 @@
+const race_id_regex = /\/(\d+)\/$/
+const race_date_regex = /(\d+)年(\d+)月(\d+)日/
+const race_schedule_regex = /(\d+)回(.+)(\d+)日目/
+// const race_course_data_regex = /(障)?([芝ダ])?([左右])?(\d+)m/
 const horse_weight_regex = /(\d+)\(([+-]?\d+)\)/
 
 module.exports = async (browser, url) => {
-	const ret = []
+	const _raceID_match = url.match(race_id_regex)
+	const raceID = _raceID_match && _raceID_match[1]
 	let isInvalid = false
+
 	const page = await browser.newPage()
 
 	await page.goto(url)
 
+	const race = {
+		id: raceID,
+		date: {
+			year: null,
+			month: null,
+			date: null
+		},
+		schedule: {
+			times: null,
+			racecourse: null,
+			day: null
+		}
+	}
+	const dataContainer = await page.$(".data_intro")
+	const raceData = await dataContainer.$eval("dl", el => {
+		const getLeafNodes = (el, array = []) => el.children.length ? [].map.call(el.children, v => getLeafNodes(v, array)).flat() : array.concat([el])
+		const leafNodes = getLeafNodes(el).map(v => v.innerText.trim())
+		const race_number = leafNodes[0] && +leafNodes[0].slice(0, -2) || null
+		const title = leafNodes[1] || null
+		const status = leafNodes[2] && leafNodes[2].split("/").map(str => str.split(":").map(v => v.trim()))
+		console.log(status[0])
+		const racecourseData = status[0] && status[0][0] && status[0][0].match(/(障)?([芝ダ])?([左右])?(\d+)m/)
+		// 晴, 曇, 小雨, 雨 ？
+		const weather = status[1][1]
+		// 良, 稍重, 重, 不良 ？
+		const condition = status[2][1]
+		const start_time = {
+			hour: status[3][1] && +status[3][1],
+			minute: status[3][2] && +status[3][2]
+		}
+		return {
+			race_number,
+			title,
+			racecourse: racecourseData && {
+				isObstacle: racecourseData[1] || false,
+				course: racecourseData[2] || null,
+				curve: racecourseData[3] || null,
+				distance: racecourseData[4] && +racecourseData[4] || null
+			} || {},
+			weather,
+			condition,
+			start_time
+		}
+	})
+	Object.assign(race, raceData)
+
+	const raceSchedule = await dataContainer.$eval(".smalltxt", el => el.innerText.split(" "))
+	const date = raceSchedule[0] && raceSchedule[0].match(race_date_regex)
+	if(date) {
+		race["date"] = {
+			year: date[1] && +date[1],
+			month: date[2] && +date[2],
+			date: date[3] && +date[3]
+		}
+	}
+	const schedule = raceSchedule[1] && raceSchedule[1].match(race_schedule_regex)
+	if(schedule) {
+		race["schedule"] = {
+			times: schedule[1] && +schedule[1],
+			racecourse: schedule[2],
+			day: schedule[3] && +schedule[3]
+		}
+	}
+
+	const raceResult = []
 	const table = await page.$("table > tbody")
 	const rows = await table.$$("tr:not(:first-child)")
 	for(const row of rows) {
@@ -100,7 +171,7 @@ module.exports = async (browser, url) => {
 					break
 				// 通過
 				case 10:
-					obj["position"] = value && value.split("-")
+					obj["position"] = value && value.split("-").map(v => +v)
 					break
 				// 上り
 				case 11:
@@ -148,7 +219,7 @@ module.exports = async (browser, url) => {
 			}
 		}
 
-		ret.push(obj)
+		raceResult.push(obj)
 	}
 
 	page.close()
@@ -157,5 +228,8 @@ module.exports = async (browser, url) => {
 		return null
 	}
 
-	return ret
+	return {
+		data: race,
+		result: raceResult
+	}
 }
